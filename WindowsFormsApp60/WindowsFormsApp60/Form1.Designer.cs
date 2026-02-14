02a42b1a14e9f9f6655a321d629a8ec2df046fef
asckord33 <maximuskrut11@gmail.com>
2026-02-14 12:50:45 +0300

    Add files via upload

---
+++ WindowsFormsApp60/WindowsFormsApp60/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace PercentCalculator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBase = new System.Windows.Forms.TextBox();
+            this.cmbMode = new System.Windows.Forms.ComboBox();
+            this.txtPercent = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBase
+            // 
+            this.txtBase.Location = new System.Drawing.Point(161, 19);
+            this.txtBase.Name = "txtBase";
+            this.txtBase.Size = new System.Drawing.Size(181, 26);
+            this.txtBase.TabIndex = 0;
+            // 
+            // cmbMode
+            // 
+            this.cmbMode.FormattingEnabled = true;
+            this.cmbMode.Location = new System.Drawing.Point(161, 63);
+            this.cmbMode.Name = "cmbMode";
+            this.cmbMode.Size = new System.Drawing.Size(181, 28);
+            this.cmbMode.TabIndex = 1;
+            // 
+            // txtPercent
+            // 
+            this.txtPercent.Location = new System.Drawing.Point(161, 109);
+            this.txtPercent.Name = "txtPercent";
+            this.txtPercent.Size = new System.Drawing.Size(181, 26);
+            this.txtPercent.TabIndex = 2;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(157, 150);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(51, 20);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Начальное число";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Выбор режима";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ввод процента";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Результат";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 203);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtPercent);
+            this.Controls.Add(this.cmbMode);
+            this.Controls.Add(this.txtBase);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBase;
+        private System.Windows.Forms.ComboBox cmbMode;
+        private System.Windows.Forms.TextBox txtPercent;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApp60/WindowsFormsApp60/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PercentCalculator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            cmbMode.Items.AddRange(new string[]
+            {
+                "От числа",
+                "На сколько % изменилось",
+                "Наценка/скидка"
+            });
+            cmbMode.SelectedIndex = 0;
+            txtBase.TextChanged += Recalculate;
+            txtPercent.TextChanged += Recalculate;
+            cmbMode.SelectedIndexChanged += Recalculate;
+        }
+
+        private void Recalculate(object sender, EventArgs e)
+        {
+            double baseValue = 0;
+            double percent = 0;
+            if (!double.TryParse(txtBase.Text, out baseValue))
+            {
+                lblResult.Text = "Введите корректное число";
+                return;
+            }
+            if (!double.TryParse(txtPercent.Text, out percent))
+            {
+                lblResult.Text = "Введите корректный процент";
+                return;
+            }
+
+            double result = 0;
+            switch (cmbMode.SelectedItem.ToString())
+            {
+                case "От числа":
+                    result = baseValue * (percent / 100);
+                    lblResult.Text = $"Процент от числа: {result:F2}";
+                    break;
+
+                case "На сколько % изменилось":
+                    result = baseValue + (baseValue * (percent / 100));
+                    lblResult.Text = $"Результат: {result:F2}";
+                    break;
+
+                case "Наценка/скидка":
+                    result = baseValue + (baseValue * (percent / 100));
+                    lblResult.Text = $"Итоговая цена: {result:F2}";
+                    break;
+
+                default:
+                    lblResult.Text = "Выберите режим";
+                    break;
+            }
+        }
+    }
+}
